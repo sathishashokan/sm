e763dfeb1b04665dd3f58f0fdab80c8347a71418
sathishashokan <40880087+sathishashokan@users.noreply.github.com>
2018-10-06 14:59:23 +0530

    Create VowelCons.java

---
+++ VowelCons.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class VowelCons {
+    public static void main(String[] args) {
+    	Scanner s = new Scanner(System.in);
+        char ch=s.next().charAt(0);
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+                {
+             System.out.println(ch+" is vowel");}
+             else
+                	System.out.println(ch+" is consonant");
+                }
+}
